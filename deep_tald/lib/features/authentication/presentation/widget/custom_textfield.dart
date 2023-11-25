e493bb21da37ce8c1e758bd0bb73981f461a23ea
Leopoldo Todisco <80098232+leotodisco@users.noreply.github.com>
2023-11-25 17:29:47 +0100

    completed login screen

--- deep_tald/lib/features/authentication/presentation/widget/custom_textfield.dart
+++ deep_tald/lib/features/authentication/presentation/widget/custom_textfield.dart
@@ -15,14 +15,22 @@ class CustomTextfield extends StatefulWidget {
 class _CustomTextfieldState extends State<CustomTextfield> {
   @override
   Widget build(BuildContext context) {
-    return TextField(
-        controller: widget.control,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(14.0)),
-          filled: true,
-          fillColor: const Color.fromARGB(255, 203, 207, 209),
-          labelText: widget.hintString,
-          labelStyle: GoogleFonts.rubik(fontSize: 16, color: Colors.black),
-        ));
+    return SizedBox(
+      height: 50,
+      width: 317,
+      child: TextField(
+          controller: widget.control,
+          decoration: InputDecoration(
+            floatingLabelBehavior: FloatingLabelBehavior.never,
+            border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(14.0)),
+            filled: true,
+            fillColor: const Color.fromARGB(166, 203, 207, 209),
+            labelText: widget.hintString,
+            labelStyle: GoogleFonts.montserrat(
+                fontSize: 16, color: const Color.fromARGB(255, 157, 164, 167)),
+          )),
+    );
   }
 }
