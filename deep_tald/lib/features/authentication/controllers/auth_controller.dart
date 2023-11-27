4e6b546d8ef244fcc92ea37f3347ef3fa7ec7530
Benedetto Scala <100568040+benedettoscala@users.noreply.github.com>
2023-11-27 22:35:24 +0100

    add different routing for medico and paziente

--- deep_tald/lib/features/authentication/controllers/auth_controller.dart
+++ deep_tald/lib/features/authentication/controllers/auth_controller.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
 import 'package:deep_tald/features/authentication/presentation/screens/initial_screen.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../../model/entity/paziente.dart';
@@ -9,6 +13,19 @@ class AuthController extends GetxController {
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   late Rx<User?> _user;
+  bool isPaziente = false;
+  late String nome;
+  late String cognome;
+
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  String getNome() {
+    return nome;
+  }
+
+  String getCognome() {
+    return cognome;
+  }
 
   //metodo che viene lanciato appena si avvia l'app
   @override
@@ -23,12 +40,46 @@ class AuthController extends GetxController {
 
   ///metodo privato che fa da route per la prima pagina
   _initialScreen(User? user) {
-    if (user == null) {
+    //get the type of user
+    //Cerca nel database se l'utente è nella collection pazienti
+    //se si allora vai alla home paziente
+    //se no vai alla home medico
+
+    if (user != null) {
+      firestore
+          .collection("Pazienti")
+          .where("uid", isEqualTo: user.uid)
+          .get()
+          .then((value) => {
+                //assegna nome e cognome
+                nome = value.docs[0]["Nome"],
+                cognome = value.docs[0]["Cognome"],
+                if (value.docs.isNotEmpty)
+                  {
+                    Get.offAllNamed(Routes.getHomePazienteRoute()),
+                  }
+                else
+                  {
+                    Get.offAllNamed(Routes.getHomeMedicoRoute()),
+                  }
+              });
+    }
+    /* if (user == null) {
       Get.offAll(() =>
           const InitialScreen()); //se al posto di login metti home ti porta alla home
     } else {
-      Get.toNamed(Routes.getHomePazienteRoute()); //quando avremo una home
-    }
+      if (isPaziente) {
+        
+      } else {
+        Get.toNamed(Routes.getHomePazienteRoute()); //quando avremo una home
+      }
+    } */
+  }
+
+  String hashPassword(String password) {
+    var hash = sha256.convert(utf8.encode(password));
+    String hashedPassword = hash.toString();
+    return hashedPassword;
   }
 
   Future<void> registerWithEmailAndPassword(
@@ -45,7 +96,7 @@ class AuthController extends GetxController {
       await auth
           .createUserWithEmailAndPassword(
             email: email,
-            password: password,
+            password: hashPassword(password),
           )
           .then((registeredUser) => {
                 firestore
@@ -62,7 +113,23 @@ class AuthController extends GetxController {
 
   Future<void> loginWithEmailPassword(String email, String password) async {
     try {
-      await auth.signInWithEmailAndPassword(email: email, password: password);
+      //print the email and password to the console
+      print("Email: $email");
+      print("Password: $password");
+      await auth.signInWithEmailAndPassword(
+          email: email, password: hashPassword(password));
+      //se il login va a buon fine
+      await firestore
+          .collection("Pazienti")
+          .where("Email", isEqualTo: email)
+          .get()
+          .then((value) => {
+                if (value.docs.isNotEmpty)
+                  {
+                    print("sono in login"),
+                    isPaziente = true,
+                  }
+              });
     } catch (e) {
       Get.snackbar(
         'Errore nel login',
