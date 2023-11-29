91e42ee25585c13494da802514245ee824235c4f
Benedetto Scala <100568040+benedettoscala@users.noreply.github.com>
2023-11-29 22:18:15 +0100

    added loading

--- deep_tald/lib/navbar/navbar_controller.dart
+++ deep_tald/lib/navbar/navbar_controller.dart
@@ -1,12 +1,17 @@
 import 'package:deep_tald/features/authentication/presentation/screens/medico_screen.dart';
 import 'package:deep_tald/features/authentication/presentation/screens/paziente_screen.dart';
 import 'package:deep_tald/features/authentication/presentation/screens/profile_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class NavbarController extends GetxController {
   var tabIndex = 0;
-  List<Widget> pagine = const [PazienteScreen(), ProfileScreen()];
+  List<Widget> pagine = const [
+    Center(
+      child: CircularProgressIndicator(),
+    )
+  ];
 
   //allo start dell'applicazione
 
