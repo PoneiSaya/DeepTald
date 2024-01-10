679294941cbd0d33da2a0447759dd209095860ed
Leopoldo Todisco <80098232+leotodisco@users.noreply.github.com>
2024-01-10 16:18:08 +0100

    added support for llama

---
+++ DeepTaldML/chatbot/tinyllama.py
@@ -0,0 +1,13 @@
+import torch
+from transformers import pipeline
+
+pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.bfloat16)
+
+testo = '''Write a simple generic question to start a chat. Write only one question'''
+
+messages = [
+    {"role": "user", "content": testo}
+]
+prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+print(outputs[0]["generated_text"])
