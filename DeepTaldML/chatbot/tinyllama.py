8a8a5f7a6ae55fca468e0587a747b50aa0bb4719
carlovend <carlovenditto28@gmail.com>
2024-01-11 16:47:08 +0100

    Co-authored-by: Leopoldo Todisco <leotodisco@users.noreply.github.com>

--- DeepTaldML/chatbot/tinyllama.py
+++ DeepTaldML/chatbot/tinyllama.py
@@ -9,10 +9,11 @@ def creaDomanda():
     messages = [
         {"role": "user", "content": testo}
     ]
+    print("sto generando una domanda")
     
     prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
     outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
-    #print(outputs[0]["generated_text"])
+    print(outputs[0]["generated_text"])
 
     return outputs[0]["generated_text"]
 
